2705794cdd0033b182a25a046b7965572df8a2b6
Saurabh Verma <saurabhshalu@gmail.com>
2020-05-23 17:27:49 +0530

    added some support in IE

--- script.js
+++ script.js
@@ -1,58 +1,84 @@
-var DesignerLeft = 50;
+// var DesignerLeft = 50;
+
+// function myText() {
+//     alert('hello ji');
+// }
+
+// function AddEvent(elem, eventname, func) {
+//     if (elem.addEventListener) {
+//         elem.addEventListener(eventname, func, false);
+//     }
+//     else if (elem.attachEvent) {
+//         elem.attachEvent('on' + eventname, func);
+//     }
+// }
+
+// function reset() {
+//     var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+//     var limitDesigner = vw / 2;
+//     document.getElementsByClassName("designer")[0].style.width = limitDesigner + 'px';
+//     document.getElementsByClassName("designerimg")[0].style.left = DesignerLeft + 'vw';
+//     document.getElementsByClassName("coderimg")[0].style.left = DesignerLeft + 'vw';
+//     document.getElementsByClassName("code")[0].style.opacity = 1;
+//     document.getElementsByClassName("design")[0].style.opacity = 1;
+// }
+
+// function hideNav() {
+//     document.getElementById('nav').checked = false;
+// }
+
+// window.onresize = function () {
+//     reset();
+// }
+
+// function codermove(e) {
+//     e = e || window.event;
+//     var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+//     var limitDesigner = vw / 2;
+//     var adjust = limitDesigner - e.clientX;
+//     if (adjust <= 0) {
+//         document.getElementsByClassName("designer")[0].style.width = (limitDesigner + (adjust / 2)) + 'px';
+//         document.getElementsByClassName("designerimg")[0].style.left = DesignerLeft + (adjust / 200) + 'vw';
+//         document.getElementsByClassName("coderimg")[0].style.left = DesignerLeft + (adjust / 200) + 'vw';
+//         document.getElementsByClassName("design")[0].style.opacity = (1 - ((e.clientX - limitDesigner) / limitDesigner));
+//     }
+//     else {
+//         reset();
+//     }
+// }
+
+// function designermove(e) {
+//     e = e || window.event;
+//     var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+//     var limitDesigner = vw / 2;
+//     var adjust = limitDesigner - e.clientX;
+//     if (adjust >= 0 && adjust <= limitDesigner) {
+//         document.getElementsByClassName("designer")[0].style.width = (limitDesigner + (adjust / 2)) + 'px';
+//         document.getElementsByClassName("designerimg")[0].style.left = DesignerLeft + (adjust / 200) + 'vw';
+//         document.getElementsByClassName("coderimg")[0].style.left = DesignerLeft + (adjust / 200) + 'vw';
+//         document.getElementsByClassName("code")[0].style.opacity = (e.clientX / limitDesigner);
+//     }
+//     else if (adjust > limitDesigner) {
+//         reset();
+//     }
+// }
 
 
-window.addEventListener("load", function(){
-    document.getElementsByClassName("designer")[0].addEventListener("mousemove", function(e) {
-        var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-        var limitDesigner = vw/2;
-        var adjust = limitDesigner - e.clientX;
-        if(adjust>=0 && adjust<=limitDesigner) {
-            document.getElementsByClassName("designer")[0].style.width = (limitDesigner + (adjust/2)) + 'px';
-            document.getElementsByClassName("designerimg")[0].style.left = DesignerLeft + (adjust/200) + 'vw';
-            document.getElementsByClassName("coderimg")[0].style.left =  DesignerLeft + (adjust/200) + 'vw';
-            document.getElementsByClassName("code")[0].style.opacity = (e.clientX/limitDesigner);
-        }
-        else if(adjust>limitDesigner) {
-            reset();
-        }
-    });
+// window.onload = function () {
 
-    document.getElementsByClassName("coder")[0].addEventListener("mousemove", function(e) {
-        var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+//     var el1 = document.getElementsByClassName("designercoder")[0];
+//     this.AddEvent(el1, 'mouseleave', reset);
 
-        var limitDesigner = vw/2;
-        var adjust = limitDesigner - e.clientX;
-        if(adjust<=0) {
-            document.getElementsByClassName("designer")[0].style.width = (limitDesigner + (adjust/2)) + 'px';
-            document.getElementsByClassName("designerimg")[0].style.left =  DesignerLeft + (adjust/200) + 'vw';
-            document.getElementsByClassName("coderimg")[0].style.left =  DesignerLeft + (adjust/200) + 'vw';
-            document.getElementsByClassName("design")[0].style.opacity = (1-((e.clientX-limitDesigner)/limitDesigner));
-        }
-        else {
-            reset();
-        }
-    });
+//     var el2 = document.getElementsByClassName("coder")[0];
+//     this.AddEvent(el2, 'mousemove', codermove);
 
-    document.getElementsByClassName("designercoder")[0].addEventListener("mouseleave", function(e) {
-        reset();
-    });
+//     var el3 = document.getElementsByClassName("designer")[0];
+//     this.AddEvent(el3, 'mousemove', designermove);
 
-    document.getElementById('loader').style.display = "none";
-});
-window.addEventListener('resize', (e) => {
-    reset();
-});
+//     if (document.getElementById('header').style.position == "fixed") {
+//         this.alert('worked');
+//     }
 
-function reset() {
-    var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-    var limitDesigner = vw/2;
-    document.getElementsByClassName("designer")[0].style.width = limitDesigner + 'px';
-    document.getElementsByClassName("designerimg")[0].style.left = DesignerLeft + 'vw';
-    document.getElementsByClassName("coderimg")[0].style.left = DesignerLeft + 'vw';
-    document.getElementsByClassName("code")[0].style.opacity = 1;
-    document.getElementsByClassName("design")[0].style.opacity = 1;
-}
+//     document.getElementById('loader').style.display = "none";
+// }
 
-function hideNav() {
-    document.getElementById('nav').checked=false;
-}
